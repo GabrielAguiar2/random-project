6cac199f8e94c12d86320d4fee1e0f99b4532900
GabrielAguiar2 <gabriel.maguiar2@gmail.com>
2024-02-05 10:02:58 -0400

    Ajustando comparações de valores

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
-const min = Math.ceil(document.querySelector("#min").value)
 
-function random(){
-    
+
+function random() {
+    const min = Math.ceil(document.querySelector("#min").value)
     const max = Math.floor(document.querySelector("#max").value)
-  
+    if(max > min){
     alert(Math.floor(Math.random() * (max - min + 1) + min));
     console.log(min, max)
-  
+    }
+
+    else{
+        alert("O primeiro número deve ser MENOR do que o segundo!")
+        window.location.reload(true);
+    }
 }
 
-function clear_all(){
+function clear_all() {
     window.location.reload(true);
 }
